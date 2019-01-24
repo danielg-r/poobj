350a072d8621e91bb0a415f9a5bdeec8e5ecaae6
danielg-r <46973585+danielg-r@users.noreply.github.com>
2019-01-24 00:44:23 -0500

    Add files via upload

---
+++ Condicionales_04-01.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04___CalculoTarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso datos
+            Console.WriteLine("Ingrese su salario completo:");
+            double salario = double.Parse(Console.ReadLine());
+            double smmlv = salario / 828116;
+
+            //Cálculo datos
+            if (smmlv <= 2)
+            {
+                Console.WriteLine("Su tarifa es A.");
+            }
+            if (2 < smmlv && smmlv < 4)
+            {
+                Console.WriteLine("Su tarifa es B.");
+            }
+            if (smmlv > 4)
+            {
+                Console.WriteLine("Su tarifa es C.");
+            }
+
+
+            //Imprimir datos
+            Console.WriteLine("Su salario en SMMLV es: " + smmlv);
+        }
+    }
+}
---
+++ Condicionales_04-03.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCondicionales
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Ingreso de datos
+            Console.WriteLine("Ingrese a, b, c.");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            //Cálculo b^2 - 4ac
+            double discriminante = Math.Pow(b, 2) - 4 * (a * c);
+
+            //Salida
+            if (discriminante == 0)
+            {
+                double x0 = - b / (2 * a);
+                Console.WriteLine("Sólo existe una solución.");
+                Console.WriteLine("X = " + x0);
+            }
+            if (discriminante > 0)
+            {
+                double x1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * (a * c))) / 2 * a;
+                double x2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * (a * c))) / 2 * a;
+                Console.WriteLine("Existen dos soluciones.");
+                Console.WriteLine("x1 = " + x1);
+                Console.WriteLine("x2 = " + x2);
+            }
+            else
+            {
+                Console.WriteLine("No existe ninguna solución");
+            }
+
+        }
+    }
+}
