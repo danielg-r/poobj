783677bc4d86653813924f1b2302ae6619c88eec
danielg-r <46973585+danielg-r@users.noreply.github.com>
2019-01-24 00:37:28 -0500

    Add files via upload

---
+++ Triangulo_02-02.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    class Triangulo2
+    {
+        static void Main()
+        {
+            //Ingreso de datos
+            Console.WriteLine("Ingrese t, a°:");
+            double t = double.Parse(Console.ReadLine());
+            double aGrados = double.Parse(Console.ReadLine());
+
+            //Deg -> Rad
+            double aRadianes = aGrados * (Math.PI / 180);
+
+            //Cálculo de los valores
+            double cRadianes = Math.PI - (Math.PI / 2) - aRadianes; //Hallamos c en radianes con 180-90-a
+            double cGrados = cRadianes * (180 / Math.PI);           //Conversión a grados
+            double z = t * Math.Sin(cRadianes);                     //Hallamos z: z=t*sin(c)
+            double y = z / Math.Tan(cRadianes);                     //Hallamos y: y=z/tan(c)
+
+            //Imprimir datos
+            Console.WriteLine("c° = " + cGrados + "°");
+            Console.WriteLine("Y = "+y);
+            Console.WriteLine("Z = "+z);
+        }
+    }
+}
---
+++ Triangulo_02-03.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo3
+{
+    class Triangulo3
+    {
+        static void Main()
+        {
+            //Ingreso de datos
+            Console.WriteLine("Ingrese c°, z:");
+            double cGrados = double.Parse(Console.ReadLine());
+            double z = double.Parse(Console.ReadLine());
+
+            //Deg -> Rad
+            double cRadianes = cGrados * (Math.PI / 180);
+
+            //Cálculo de los valores
+            double y = z / Math.Tan(cRadianes);                     //Hallamos y: y=z/tan(c)
+            double t = Math.Sqrt(Math.Pow(y, 2) + Math.Pow(z, 2));  //Pitágoras t^2 = y^2 + z^2
+            double aRadianes = Math.PI - (Math.PI / 2) - cRadianes; //Hallamos a en radianes con 180-90-c
+            double aGrados = aRadianes * (180 / Math.PI);           //Conversión a grados
+
+            //Imprimir datos
+            Console.WriteLine("a° = " + aGrados + "°");
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("t = " + t);
+        }
+    }
+}
